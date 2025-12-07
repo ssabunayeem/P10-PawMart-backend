2e3139c16ed9a8f6ad117d8c7c01a60fe819247e
ssabunayeem <ssabunayeem@gmail.com>
2025-12-08 02:30:44 +0600

    done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const database = client.db('petService');
         const petServices = database.collection('services');
@@ -109,7 +109,7 @@ async function run() {
         // Send a ping to confirm a successful connection
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
